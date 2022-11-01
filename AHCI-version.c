339b31336c5113a1a3e662cd2e48a52483ce7520
jv <combrogi.viejo@gmail.com>
2022-11-01 10:12:01 +0100

    AHCI-version.c done

--- AHCI-version.c
+++ AHCI-version.c
@@ -7,17 +7,11 @@
 #include <unistd.h>
 #include <stdint.h>
 
-// Dirección base por defecto de registros MMIO de Local-APIC (/proc/iomem)
-#define LOCAL_APIC_BASE_ADDRESS 0xfee00000
-
 //estructura de datos donde meter los datos del AHCI
 struct lapic_info {
-	uint32_t reservado[8];
+	uint32_t reservado[4];
 	uint16_t minor;
 	uint16_t major;
-	//uint32_t ID;
-	//uint32_t relleno[3];
-	//uint32_t version; // nº versión: 8 bits de menor peso
 };
 
 int main(int argc, char *argv[]) {
@@ -28,7 +22,11 @@ int main(int argc, char *argv[]) {
 		return -1;
 	}
 
+	//variables locales
+	int fd, tam=4096, major = 0, minor = 0;
+	struct lapic_info volatile *lapic;
 	unsigned long num = 0;
+	
 	//tratar valor introducido
 	if (argv[1][0] == '0' && argv[1][1] == 'x') {
 		//hexadecimal
@@ -41,12 +39,8 @@ int main(int argc, char *argv[]) {
 		num = strtoul(argv[1],NULL,0);
 	}
 
-	printf("numero introducido: %lx\n",num);
-
 	//abrir fichero /dev/mem
-	int fd, tam=4096;
-	struct lapic_info volatile *lapic; // volatile elimina optimizaciones de compilador
-	if ((fd = open("/dev/mem", O_RDONLY|O_DSYNC)) < 0) { // O_DSYNC: accesos no usan cache
+	if ((fd = open("/dev/mem", O_RDONLY|O_DSYNC)) < 0) {
 		perror("open"); 
 		return 1; 
 	}
@@ -57,10 +51,22 @@ int main(int argc, char *argv[]) {
 		return 1; 
 	}
 
+	//obtener datos relevantes
+	major = lapic->major;
+	minor = 0;
 
-	printf("Acceso dir física %lx usando %p\n", num, lapic);
-	printf("test minor: %d major: %d\n", lapic->minor, lapic->major);
-	//printf("Local APIC ID: %d Versión %d\n", lapic->ID, lapic->version&0xFF);
+	//filtrar minor
+	switch (lapic->minor) {
+		case 0x905: minor = 95; break;
+		case 0x100: minor = 1; break;
+		case 0x200: minor = 2; break;
+		case 0x300: minor = 3; break;
+		case 0x301: minor = 3.1; break;
+	}
+
+
+	//printf("Acceso dir física %lx usando %p\n", num, lapic);
+	printf("Versión %d.%d\n", major, minor);
 
 	close(fd); 
 	munmap((void *)lapic, tam);
