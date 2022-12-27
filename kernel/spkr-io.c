ad509680f09311c77bc22421a2717357d10f2fca
jv <combrogi.viejo@gmail.com>
2022-12-27 17:11:30 +0100

    fase 1

--- kernel/spkr-io.c
+++ kernel/spkr-io.c
@@ -2,14 +2,73 @@
 #include <linux/module.h>
 #include <linux/version.h>
 #include <linux/i8253.h>
+#include <asm/io.h>
 
+//declaracion de funciones
+/*void set_spkr_frequency(unsigned int frequency);
+void spkr_on(void);
+void spkr_off(void);*/
+
+
+///////////////////////////////////////////////////////
+/*implementacion*/
+//////////////////////////////////////////////////////
+
+//fijar frecuencia
 void set_spkr_frequency(unsigned int frequency) {
+	//variables locales
+	uint32_t port1 = 0x42;
+	uint32_t port2 = 0x43;
+	int freq = PIT_TICK_RATE / frequency;
+	unsigned long flags = 0;
+
 	printk(KERN_INFO "spkr set frequency: %d\n", frequency);
+
+
+	//inicio seccion critica
+	raw_spin_lock_irqsave(&i8253_lock, flags);
+	
+	//escritura en el dispositivo
+	outb_p(freq & 0xff, port1);
+	outb((freq >> 8) & 0xff, port2);
+
+	//fin seccion critica
+	raw_spin_unlock_irqrestore(&i8253_lock, flags);
 }
 
+//encender altavoz
 void spkr_on(void) {
+	//variables locales
+	uint32_t port = 0x61;
+	unsigned long flags = 0;
+
+	//inicio seccion critica
+	raw_spin_lock_irqsave(&i8253_lock, flags);
+	
+	//activar mute
+	outb(inb(port) | 3, port);
+
+	//fin seccion critica
+	raw_spin_unlock_irqrestore(&i8253_lock, flags);
+
 	printk(KERN_INFO "spkr ON\n");
+
 }
+
+//apagar altavoz
 void spkr_off(void) {
+	//variables locales
+	uint32_t port = 0x61;
+	unsigned long flags = 0;
+
+	//inicio seccion critica
+	raw_spin_lock_irqsave(&i8253_lock, flags);
+	
+	//desactivar mute
+	outb(inb_p(port) & 0xFC, port);
+
+	//fin seccion critica
+	raw_spin_unlock_irqrestore(&i8253_lock, flags);
+
 	printk(KERN_INFO "spkr OFF\n");
 }
--- kernel/spkr-main.c
+++ kernel/spkr-main.c
@@ -10,9 +10,37 @@
 #include <linux/ioctl.h>
 #include <asm/uaccess.h>
 
+//datos del modulo
 MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Juan Alfonso Viejo Rodriguez <juanalfonso.viejo.rodriguez@alumnos.upm.es>");
+MODULE_DESCRIPTION("sound driver for pc");
+MODULE_VERSION("0.0.1");
 
+//declaracion de funciones
 extern void set_spkr_frequency(unsigned int frequency);
 extern void spkr_on(void);
 extern void spkr_off(void);
 
+//variables globales
+static int frecuencia = 0;
+module_param(frecuencia, int, S_IRUGO);
+
+/////////////////////////////////////////////////////////////////////////
+/*Coidgo*/
+/////////////////////////////////////////////////////////////////////////
+
+static int __init init_initpkr(void) {
+    printk("Inicio del driver\n");
+    set_spkr_frequency(frecuencia);
+    spkr_on();
+    return 0;
+}
+
+static void __exit exit_intpkr(void) {
+    printk("fin del modulo\n");
+    spkr_off();
+}
+
+
+module_init(init_initpkr);
+module_exit(exit_intpkr);
