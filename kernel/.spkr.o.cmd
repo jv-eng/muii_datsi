8f1ab374f50946a12460d3eb3d402cd67b702775
jv <combrogi.viejo@gmail.com>
2022-12-28 20:38:12 +0100

    fase 3.5

--- kernel/.spkr.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/jv/muii_datsi/kernel/spkr.o := ld -m elf_x86_64 -z noexecstack   -r -o /home/jv/muii_datsi/kernel/spkr.o /home/jv/muii_datsi/kernel/spkr-main.o /home/jv/muii_datsi/kernel/spkr-io.o
